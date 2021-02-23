aef1a26bb83f6c8152903d5b4baf28da5af8c673
Rafael Barreto Daldegan <ra.fa.daldegan@hotmail.com>
2021-02-22 21:14:06 -0300

    Mudança pro modo blocking no fallback

--- pages/index.js
+++ pages/index.js
@@ -16,40 +16,47 @@ const Container = styled.div`
 `;
 
 const categories = [
-  { slug: 'PROMOCAO', title: 'Promoções', subTitle: 'O melhor custo benefício pra você' },
+  { slug: 'promocao', title: 'Promoções', subTitle: 'O melhor custo benefício pra você' },
   { slug: 'TODAS-AS-CATEGORIAS', title: 'Variedades', subTitle: 'Tudo o que você puder desejar' },
 ];
 
 export async function getStaticProps() {
+  const errors = [];
+
   const categoriesList = categories.map(async (categorie) => ({
     header: categorie,
     items: await commerce.products.list({
       limit: 4,
       category_slug: categorie.slug,
-    }),
+    })
+      .catch((error) => errors.push(error)),
   }));
 
   const data = await Promise.all(categoriesList);
 
+  console.log('#############################', errors);
+
   return {
     props: {
       data,
+      errors,
     },
     revalidate: 10800,
   };
 }
 
-export default function Home({ data }) {
+export default function Home({ data, errors }) {
+  console.log(errors);
   return (
     <Container>
       <Carousel />
-      {data.map((categorie) => (
+      {/* {data.map((categorie) => (
         <ItemsCategorie
           key={categorie.header.slug}
           header={categorie.header}
           products={categorie.items}
         />
-      ))}
+      ))} */}
       {/* <InstagramFeed /> */}
     </Container>
   );
